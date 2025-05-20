51ffdb58b2d09e43d41c49254dee2a2c7901c4be
delta student <reter3246081@gmail.com>
2025-05-20 15:47:10 +0530

    commited

--- app.js
+++ app.js
@@ -85,10 +85,6 @@ app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
 
-app.get('/', (req, res) => {
-  res.redirect('/listings');
-});
-
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong!" } = err;
